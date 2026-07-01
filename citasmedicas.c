c0ce4d57bb4deedb90efff3935ebe93d5a1ee571
Carlos Salazar <carlos.salazar.camacho@udla.edu.ec>
2026-07-01 17:12:04 -0500

    Creacion del menu

--- citasmedicas.c
+++ citasmedicas.c
@@ -1,5 +1,53 @@
 #include <stdio.h>
+void MostrarMenu();
+    int main(){
+        int opcion;
+        do{
+            MostrarMenu();
+            scanf("%d", &opcion);
+            switch (opcion){
+                case 1:
+                printf("\nRegistrar cita\n");
+                break;
+                case 2:
+                printf("\nListar cita\n");
+                break;
+                case 3:
+                printf("\nBuscar cita\n");
+                break;
+                case 4:
+                printf("\nActualizar cita\n");
+                break;
+                case 5:
+                printf("\n Eliminar cita\n");
+                break;
+                case 6:
+                printf("\n Guardar cambios de la cita\n");
+                break;
+                case 7:
+                printf("\nSalida\n");
+                break;
 
+
+            }
+            
+            
+        }while(opcion !=7);
+        return 0;
+    }
+void MostrarMenu(){
+            printf("\n==========================================\n");
+            printf("/ BIENVENIDO AL SISTEMA DE CITAS MEDICAS /\n");
+            printf("==========================================\n");
+            printf("1. Registrar cita\n");
+            printf("2.Listar cita\n");
+            printf("3.Buscar cita\n");
+            printf("4.Actualizar cita\n");
+            printf("5.Eliminar cita\n");
+            printf("6. Guardar cambios de la cita\n");
+            printf("7. Salir\n");
+            printf("Seleccione una opción: ");
+}
 typedef struct {
     char codigo_cita[16];
     char nombre_paciente[50];
---
+++ pruebas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int opcion;
+        printf("\n==========================================\n");
+        printf("/ BIENVENIDO AL SISTEMA DE CITAS MEDICAS /\n");
+        printf("==========================================\n");
+        printf("1. Registrar cita\n");
+        printf("2.Listar cita\n");
+        printf("3.Buscar cita\n");
+        printf("4.Actualizar cita\n");
+        printf("5.Eliminar cita\n");
+        printf("6. Guardar cambios de la cita\n");
+        printf("7. Salir\n");
+        printf("Seleccione una opción: ");
+        scanf("%d", &opcion);
+}
